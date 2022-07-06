b9cdf0dea8e726f436fbebfc6b07b456b2901e70
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2022-07-06 14:16:52 +0200

    Improve icon showcase

--- src/main/java/pl/pkrysztofiak/controlsshowcase/icon/IconShowcase.java
+++ src/main/java/pl/pkrysztofiak/controlsshowcase/icon/IconShowcase.java
@@ -3,7 +3,9 @@ package pl.pkrysztofiak.controlsshowcase.icon;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class IconShowcase extends Application {
@@ -14,11 +16,23 @@ public class IconShowcase extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
         Button button = new Button("Hello");
         button.setGraphic(new Icon("m 5.8528867,11.731844 c 1.1471133,0 2.2160149,-0.33892 3.1154557,-0.912477 L 12.148976,14 14,12.148976 10.819367,8.981378 C 11.392924,8.0689016 11.731844,7.0130356 11.731844,5.8659214 11.731844,2.6201117 9.111732,0 5.8659214,0 2.6201117,0 0,2.6201117 0,5.8659214 0,9.111732 2.6201117,11.731844 5.8528867,11.731844 Z m 0.013035,-9.1247677 c 1.7988834,0 3.2588461,1.4599626 3.2588461,3.2588451 0,1.7988834 -1.4599627,3.2588462 -3.2588461,3.2588462 -1.7988825,0 -3.2588451,-1.4599628 -3.2588451,-3.2588462 0,-1.7988825 1.4599626,-3.2588451 3.2588451,-3.2588451 z"));
-        button.setStyle("-fx-font-size: 4em");
 
-        stage.setScene(new Scene(new StackPane(button), 400, 400));
+        StackPane stackPane = new StackPane(button);
+        stackPane.setPrefSize(400, 400);
+
+        Slider slider = new Slider(0.25, 6, 1);
+        slider.valueProperty().addListener((observableValue, oldFontSize, fontSize) -> {
+            button.setStyle("-fx-font-size: " + fontSize + "em;");
+        });
+
+        VBox vBox = new VBox(stackPane, slider);
+
+        stage.setScene(new Scene(vBox, 400, 400));
         stage.show();
+
+        slider.requestFocus();
     }
 }
